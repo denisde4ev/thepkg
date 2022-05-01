f77460c1de7e3b0f64d65d53e52e36c930b93560
denisde4ev <40498286+denisde4ev@users.noreply.github.com>
2022-05-01 12:33:55 +0300

    Create thepkg-install.sh

---
+++ thepkg-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# curl https://github.com/denisde4ev/thepkg/raw/master/thepkg-install.sh | THEPKG_PREFIX=/ THEPKG_DBPATH=/var/db/thepkg sh -x
+
+(
+set -eu
+
+: "${THEPKG_PREFIX:?}"
+: "${THEPKG_DBPATH:?}"
+
+mkdir -pv "${THEPKG_PREFIX}/bin"
+mkdir -pv "${THEPKG_DBPATH}/thepkg"
+
+
+curl -L https://github.com/denisde4ev/thepkg/raw/master/thepkg > "${THEPKG_PREFIX}/bin/thepkg"
+chmod 755 "${THEPKG_PREFIX}/bin/thepkg"
+
+# todo: patch the prefis and dbpath of thepkg
+patch --forward --strip=1 thepkg << EOF
+--- thepkg.old	2022-05-01
++++ thepkg.new	2022-05-01
+@@ -4,2 +4,2 @@
+-: "\${THEPKG_PREFIX:=./opt}"
+-: "\${THEPKG_DBPATH:=\$THEPKG_PREFIX/../var/db/thepkg}"
++: "\${THEPKG_PREFIX:='$THEPKG_PREFIX'}"
++: "\${THEPKG_DBPATH:='$THEPKG_PREFIX'}"
+EOF
+
+printf %s\\n "${THEPKG_PREFIX}/bin/thepkg" >> "${THEPKG_DBPATH}/thepkg/manifest"
+)
